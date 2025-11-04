bb1d0b23d059ca227d347ca43ab2030a3ff8c676
Sibuchanda <sibuchanda457@gmail.com>
2025-11-04 19:26:21 +0530

    Redux : 01 (Created redux store , reducers and dispatch an action into the store )

--- Concept_Rewind/src/redux/store.jsx
+++ Concept_Rewind/src/redux/store.jsx
@@ -37,8 +37,16 @@ const store = createStore(taskReducer);
 console.log(store);
 console.log("Initial state is : ", store.getState());
 
+
+// Action creators
+const addTask = (data)=>{
+    return {type: ADD_TASK, payload: data};
+}
+
 // Now we want to add any data into the store. For that we have to call the dispatch method
 store.dispatch({type: ADD_TASK, payload: "Buy banana from market"});
+//Adding data using 'action creators' this is more convenient then the above method
+store.dispatch(addTask("Buy apple from the market"));
 console.log("Updated state is : ", store.getState());
 
 store.dispatch({type: DELETE_TASK, payload: 0});  // We passed 0 to delete 0 index data from the task array
