814c91d613662d5a6a953d74574f1e82da48b41e
Sibuchanda <sibuchanda457@gmail.com>
2025-11-04 19:02:45 +0530

    Redux : 01 (Created redux store , reducers and dispatch an action into the store )

---
+++ Concept_Rewind/src/redux/store.jsx
@@ -0,0 +1,45 @@
+import {createStore} from 'redux'; 
+
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
+
+const initialState = {
+    task: [],
+    isLoading: false,
+}
+
+
+const taskReducer = (state=initialState, action )=>{
+    switch(action.type) {
+        case ADD_TASK: 
+            return {
+                ...state,
+                task: [...state.task, action.payload],
+            };
+
+        case DELETE_TASK:
+            const updatedTask = state.task.filter((curTask, index)=>{
+                return index !== action.payload;
+            });
+             return{
+                ...state,
+                task: updatedTask,
+             };
+
+        default: 
+          return state;
+    }
+};
+
+
+
+const store = createStore(taskReducer);
+console.log(store);
+console.log("Initial state is : ", store.getState());
+
+// Now we want to add any data into the store. For that we have to call the dispatch method
+store.dispatch({type: ADD_TASK, payload: "Buy banana from market"});
+console.log("Updated state is : ", store.getState());
+
+store.dispatch({type: DELETE_TASK, payload: 0});  // We passed 0 to delete 0 index data from the task array
+console.log("After Delete state is : ", store.getState());
